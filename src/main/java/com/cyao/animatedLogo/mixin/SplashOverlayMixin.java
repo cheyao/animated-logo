8ce5fdb39e940d32f6786e917367004c338e688d
nellieldev <152215399+nellieldev@users.noreply.github.com>
2025-08-11 23:04:48 +0200

    👂 Fixed earrape so I won't need a 🦻 in the future

--- src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
+++ src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
@@ -1,32 +1,24 @@
 package com.cyao.animatedLogo.mixin;
 
-import com.cyao.animatedLogo.AnimatedLogo;
-import com.cyao.animatedLogo.util.DoneLineListen;
-import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.gl.RenderPipelines;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.SplashOverlay;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.resource.ResourceReload;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.ColorHelper;
-import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.llamalad7.mixinextras.sugar.*;
+import net.minecraft.client.*;
+import net.minecraft.client.gl.*;
+import net.minecraft.client.gui.*;
+import net.minecraft.client.gui.screen.*;
+import net.minecraft.resource.*;
+import net.minecraft.sound.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.*;
 
 import javax.sound.sampled.*;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.io.*;
 
 @Mixin(SplashOverlay.class)
 public class SplashOverlayMixin {
+
     @Shadow
     @Final
     private ResourceReload reload;
@@ -36,7 +28,9 @@ public class SplashOverlayMixin {
     private long reloadStartTime = -1L;
     @Shadow
     private float progress;
-    @Shadow @Final private boolean reloading;
+    @Shadow
+    @Final
+    private boolean reloading;
     @Unique
     private int count = 0;
     @Unique
@@ -53,8 +47,6 @@ public class SplashOverlayMixin {
     private float f = 0;
     @Unique
     private boolean fast = false;
-    @Unique
-    private final AtomicBoolean playing = new AtomicBoolean(false);
     @Unique
     private boolean done = false;
     @Unique
@@ -103,29 +95,25 @@ public class SplashOverlayMixin {
                 this.frames[i] = Identifier.of("animated-logo", "textures/gui/frame_" + i + ".png");
             }
 
-            if(!reloading) {
-                InputStream finalS = AnimatedLogo.class.getResourceAsStream("/logo.wav");
-                if (finalS != null) {
-                    playing.set(true);
-                    new Thread(new Runnable() {
-                        public void run() {
-                            try {
-                                Clip clip = AudioSystem.getClip();
-                                AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                                        new ByteArrayInputStream(finalS.readAllBytes()));
-                                clip.open(inputStream);
-                                clip.addLineListener(new DoneLineListen(playing));
-                                startTime = System.currentTimeMillis();
-                                clip.start();
-                            } catch (Exception e) {
-                            }
-                            try {
-                                finalS.close();
-                            } catch (Exception e) {
-                            }
+            if (!reloading) {
+                final MinecraftClient client = MinecraftClient.getInstance();
+                final ResourceManager resourceManager = client.getResourceManager();
+                final Identifier soundId = Identifier.of("animated-logo", "logo.wav");
+
+                resourceManager.getResource(soundId).ifPresent(resource -> new Thread(() -> {
+                    try (final InputStream inputStream = resource.getInputStream(); final BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream); final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream)) {
+                        final Clip clip = AudioSystem.getClip();
+                        clip.open(audioInputStream);
+                        { // Calculate gain from minecraft's master volume
+                            final FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                            final float masterVolume = MinecraftClient.getInstance().options.getSoundVolume(SoundCategory.MASTER);
+                            gainControl.setValue(masterVolume > 0.0 ? Math.min(gainControl.getMaximum(), Math.max(gainControl.getMinimum(), (float) (Math.log10(masterVolume) * 20.0))) : gainControl.getMinimum());
                         }
-                    }, "SoundPlayer").start();
-                }
+                        this.startTime = System.currentTimeMillis();
+                        clip.start();
+                    } catch (final Exception ignored) {
+                    }
+                }, "SoundPlayer").start());
             }
 
             inited = true;
@@ -140,18 +128,22 @@ public class SplashOverlayMixin {
 
         float progress = MathHelper.clamp(this.progress * 0.95F + this.reload.getProgress() * 0.050000012F, 0.0F, 1.0F);
 
-        context.drawTexture(RenderPipelines.MOJANG_LOGO, this.frames[count / IMAGE_PER_FRAME / FRAMES_PER_FRAME], x - halfWidth, y - halfHeight,
-                0, 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME), (int) width, (int) height, 1024, 256, 1024, 1024, ColorHelper.getWhite(alpha));
+        context.drawTexture(
+                RenderPipelines.MOJANG_LOGO, this.frames[count / IMAGE_PER_FRAME / FRAMES_PER_FRAME], x - halfWidth, y - halfHeight,
+                0, 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME), (int) width, (int) height, 1024, 256, 1024, 1024, ColorHelper.getWhite(alpha)
+        );
 
         if (progress >= 0.8) {
             f = Math.min(alpha, f + 0.2f);
 
             int sw = (int) (width * 0.45);
-            context.drawTexture(RenderPipelines.MOJANG_LOGO, Identifier.of("animated-logo", "textures/gui/studios.png"), x - sw / 2, (int) (y - halfHeight + height - height / 12),
-                    0, 0, sw, (int) (height / 5.0), 450, 50, 512, 512, ColorHelper.getWhite(f));
+            context.drawTexture(
+                    RenderPipelines.MOJANG_LOGO, Identifier.of("animated-logo", "textures/gui/studios.png"), x - sw / 2, (int) (y - halfHeight + height - height / 12),
+                    0, 0, sw, (int) (height / 5.0), 450, 50, 512, 512, ColorHelper.getWhite(f)
+            );
         }
 
-        if(reloading) {
+        if (reloading) {
             if (count != FRAMES * IMAGE_PER_FRAME * FRAMES_PER_FRAME - 1) {
                 count++;
 
